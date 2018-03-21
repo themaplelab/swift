7eb55ffe722b0cbf3746b7e843db762a1008f8ce
Leeleo3x <leeleo3x@gmail.com>
2018-03-20 21:02:32 -0700

    Support partial_apply instruction.

--- include/swift/WALASupport/SILWalaInstructionVisitor.h
+++ include/swift/WALASupport/SILWalaInstructionVisitor.h
@@ -47,6 +47,7 @@ public:
   jobject visitTryApplyInst(TryApplyInst *TAI);
   jobject visitBeginApplyInst(BeginApplyInst *BAI);
   jobject visitFloatLiteralInst(FloatLiteralInst *FLI);
+  jobject visitPartialApplyInst(PartialApplyInst *PAI);
 
 private:
   void updateInstrSourceInfo(SILInstruction *I);
