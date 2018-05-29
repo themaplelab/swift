6f50bc78afb98959a62fb4a52a2eacae9ab40916
Karim Ali <karim.ali@ualberta.ca>
2018-05-29 16:36:24 -0600

    Revert "added support for the Switch Enum Instruction"

--- include/swift/WALASupport/SILWalaInstructionVisitor.h
+++ include/swift/WALASupport/SILWalaInstructionVisitor.h
@@ -46,7 +46,7 @@ public:
   jobject visitBranchInst(BranchInst *BI);
   jobject visitCondBranchInst(CondBranchInst *CBI);
   jobject visitSwitchValueInst(SwitchValueInst *SVI);
-  jobject visitSwitchEnumInst(SwitchEnumInst *SWI);
+  jobject visitSelectValueInst(SelectValueInst *SVI);
   jobject visitEnumInst(EnumInst *EI);
   jobject visitUnreachableInst(UnreachableInst *UI);
   jobject visitCopyValueInst(CopyValueInst *CVI);
