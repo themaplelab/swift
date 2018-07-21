683a00966c111a7e691487c7764d4929b733514e
Mark Mroz <markmroz95@gmail.com>
2018-07-21 11:23:16 -0400

    fix removing method from header

--- include/swift/WALASupport/SILWalaInstructionVisitor.h
+++ include/swift/WALASupport/SILWalaInstructionVisitor.h
@@ -100,8 +100,8 @@ jobject visitValueMetatypeInst(ValueMetatypeInst *VMI);
 jobject visitCopyValueInst(CopyValueInst *CVI);
 jobject visitTupleInst(TupleInst *TI);
 jobject visitStructInst(StructInst *SI);
-jobject visitStructElementAddrInst(StructElementAddrInst *SEAI);
 jobject visitStructExtractInst(StructExtractInst *SEI);
+jobject visitRefElementAddrInst(RefElementAddrInst *REAI);
 
 /*******************************************************************************/
 /*                          Enums                                              */
